fa4cdb57b49eead71ac13749b210f474ddc574a2
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-02-14 20:23:07 -0300

    CreateDeliveryUseCase and ensuring user is authenticated to create a delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { ensureAuthenticateClient } from "./modules/middlewares/ensureAuthenticateClient";
 
 const routes = Router();
 
@@ -11,11 +13,14 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
 
 routes.post("/client/", createClientController.handle)
 routes.post("/deliveryman/", createDeliverymanController.handle)
+
+routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
 
 export { routes };
