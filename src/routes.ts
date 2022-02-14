51d9acb39c1960cd4b76cecbdd3ccdcb43e4ae55
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-02-14 16:31:17 -0300

    AuthenticateDeliverymanUseCase

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/useCases/authenticateClient/AuthenticateClientController";
+import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
+const authenticateClientController = new AuthenticateClientController();
+const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
-const authenticateClientController = new AuthenticateClientController();
+
+routes.post("/client/authenticate/", authenticateClientController.handle)
+routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
 
 routes.post("/client/", createClientController.handle)
 routes.post("/deliveryman/", createDeliverymanController.handle)
-routes.post("/authenticate/", authenticateClientController.handle)
 
 export { routes };
