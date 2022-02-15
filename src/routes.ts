ce0dd4a475ac173553186899befee9f8a4ca0fcb
Bernardo Pretto <bernardodgpretto@gmail.com>
2022-02-15 09:52:28 -0300

    routing FindAllAvailableDeliveriesUseCase

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import { AuthenticateClientController } from "./modules/account/useCases/authent
 import { AuthenticateDeliverymanController } from "./modules/account/useCases/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
+import { FindAllAvailableDeliveriesController } from "./modules/deliveries/useCases/findAllAvailableDeliveries/FindAllAvailableDeliveriesController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 import { ensureAuthenticateClient } from "./modules/middlewares/ensureAuthenticateClient";
 
@@ -14,6 +15,7 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailableDeliveriesController = new FindAllAvailableDeliveriesController();
 
 routes.post("/client/authenticate/", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate/", authenticateDeliverymanController.handle)
@@ -22,5 +24,6 @@ routes.post("/client/", createClientController.handle)
 routes.post("/deliveryman/", createDeliverymanController.handle)
 
 routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.handle)
+routes.get("/delivery/available", findAllAvailableDeliveriesController.handle)
 
 export { routes };
